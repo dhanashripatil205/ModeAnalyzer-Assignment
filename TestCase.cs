18a387841de632b9139de98336357cdd09e8f20f
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-17 23:56:41 +0530

    creating a class MoodAnalyzerNullException.

--- TestCase.cs
+++ TestCase.cs
@@ -1,65 +1,18 @@
-using ModeAnalyzerAssignment;
-using System.Diagnostics.SymbolStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace TestCaseMoodAnalyzer
+namespace ModeAnalyzerAssignment
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestCase1_1()
+  
+        public class MoodAnalyzerNullException : Exception
         {
-            string input="I am in Sad Mood";
-            string expected = "SAD";
+            public MoodAnalyzerNullException(string msg) : base(msg)
+            {
 
-
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-            
-          string actual= moodAnalyzer.analyseMood(input);
-
-            Assert.AreEqual(expected, actual);
+            }
         }
-
-        [TestMethod]
-        public void TestCase1_2()
-        {
-            string input = "I am in Any Mood";
-            string expected = "HAPPY";
-
-
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-
-            string actual = moodAnalyzer.analyseMood(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Refactor1_1()
-        {
-            string input = "I am in Sad Mood";
-            string expected = "SAD";
-
-
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-
-            string actual = moodAnalyser.msg;
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Refactor1_2()
-        {
-            string input = "I am in Happy Mood";
-            string expected = "SAD";
-
-
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-
-            string actual = moodAnalyser.msg;
-
-            Assert.AreEqual(expected, actual);
-        }
-    }
+    
 }

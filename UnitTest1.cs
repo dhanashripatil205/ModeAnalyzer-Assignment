385669c9b8cb064dd3084657f0db83c04068fb41
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-18 20:32:51 +0530

    Unit Test 5.1 Is updated!!

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,24 +1,24 @@
 using ModeAnalyzerAssignment;
 //using MoodAnalyzer;
 using System.Diagnostics.SymbolStore;
+using System.Reflection;
 
 namespace TestCaseMoodAnalyzer
 {
-   
- [TestClass]
+    [TestClass]
     public class UnitTest1
     {
-     [TestMethod]
-        public void UseCase4_3()
+       
+
+        [TestMethod]
+        public void UseCase5_1()
         {
-            try
-            {
-                object returned = Mood_Aanalyzer_Factory.CreateMoodAnalyze("ModeAnalyzerAssignment.MoodAnalyzer", "MoodAnalyz");
-            }
-            catch (MoodAnalyzerNullException Exception)
-            {
-                Assert.AreEqual("Constructor not found", Exception.Message);
-            }
+            object expected = new MoodAnalyser();
+            object returned = Mood_Aanalyzer_Factory.CreateMoodAnalyze_Parameter_Constructor("ModeAnalyzerAssignment.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(returned);
         }
+
+     
+
     }
 }
